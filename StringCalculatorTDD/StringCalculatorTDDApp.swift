5bbed72d83e6f5e7ce26df61bb1788b7332246a6
Pintu Dodeja <dodeja.pintu@gmail.com>
2025-07-04 15:15:38 +0530

    Initial commit

---
+++ StringCalculatorTDD/StringCalculatorTDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringCalculatorTDDApp.swift
+//  StringCalculatorTDD
+//
+//  Created by Pintu Dodeja on 04/07/25.
+//
+
+import SwiftUI
+
+@main
+struct StringCalculatorTDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
